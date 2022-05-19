f23a08fa3cb0f5bd7070a8d4f2c2e0c9fb82222b
LENOVO <nrtechnologyid@gmail.com>
2022-05-19 16:30:46 +0700

    yolo ignore

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ for i in range(len(boxes)):
         label = str(classes[class_ids[i]])
         color = colors[i]
         cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+        # cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
